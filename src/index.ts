407dd7cd2f3a4b2c8ed5ce4ed890a140e8bc664a
Juhodev <jooo.dev@gmail.com>
2020-10-11 12:42:15 +0300

    Remove dead code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-// const Discord = require('discord.js');
-// const CommandHandler = require('./commandHandler');
-// const QuoteCommand = require('./commands/quoteCommand');
-// const DB = require('./db');
-// require('dotenv').config();
-
-// const client = new Discord.Client();
-
-// const db = new DB();
-// db.load();
-
-// const commandHandler = new CommandHandler(client, db);
-// commandHandler.registerCommand(QuoteCommand);
-
-// client.on('ready', () => {
-// 	console.log('Connected');
-// });
-
-// client.on('message', (message) => {
-// 	commandHandler.handle(message);
-// });
-
-// client.login(process.env.DISCORD_TOKEN);
-
 import * as Discord from 'discord.js';
 import CommandHandler from './commandHandler';
 import DB from './database/db';
