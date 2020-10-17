eb5f1f5e8027a9934a348c674b54cae51ae0ade2
Juhodev <jooo.dev@gmail.com>
2020-10-17 11:25:21 +0300

    Update user times

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,6 @@
-export function msToTime(duration: number) {
-	let milliseconds = Math.round((duration % 1000) / 100),
-		seconds: string | number = Math.floor((duration / 1000) % 60),
-		minutes: string | number = Math.floor((duration / (1000 * 60)) % 60),
-		hours: string | number = Math.floor((duration / (1000 * 60 * 60)) % 24);
+export function msToTime(ms: number) {
+	const hours = Math.floor((ms / (1000 * 60 * 60)) % 60);
+	const minutes = Math.floor((ms / (1000 * 60)) % 60);
 
 	return `${hours} hours ${minutes} minutes`;
 }
