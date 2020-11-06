297306ab7574b61944e9adb9b8137f4159ee448a
Juhodev <jooo.dev@gmail.com>
2020-11-06 19:02:29 +0200

    Fix millisecond formatting

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,7 @@ import { uuid } from 'uuidv4';
 const streamPipeline = util.promisify(require('stream').pipeline);
 
 export function msToTime(ms: number) {
-	const hours = Math.floor((ms / (1000 * 60 * 60)) % 60);
+	const hours = Math.floor((ms / (1000 * 60 * 60)));
 	const minutes = Math.floor((ms / (1000 * 60)) % 60);
 
 	return `${hours} hours ${minutes} minutes`;
