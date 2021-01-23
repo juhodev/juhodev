6c6b5d6813460c67fd4125ff9b6a82364aba02cc
juhodev <juho.seppl@gmail.com>
2021-01-23 23:00:15 +0200

    finish writing the LFU cache

--- src/cache/LFUCache.ts
+++ src/cache/LFUCache.ts
@@ -20,7 +20,20 @@ class LFUCache {
 		return this.size;
 	}
 
+	/**
+	 * Inserts a new cache node in the cache.
+	 *
+	 * If this would push to cache size over the max size of the cache then
+	 * this will remove the least frequently used item.
+	 *
+	 * @param key The key with which the value can later be retrieved with.
+	 * @param value The value that is stored in the cache.
+	 */
 	insert(key: string, value: any) {
+		if (this.size >= this.maxSize) {
+			this.removeLeastUsed();
+		}
+
 		this.size++;
 		if (this.frequencyList.getSize() === 0) {
 			// If the `frequencyList` is empty then create a new frequency node
@@ -35,6 +48,15 @@ class LFUCache {
 		}
 	}
 
+	/**
+	 * Tries to retrieve the value with the given `key` from cache.
+	 *
+	 *  This will handle incrementing the cache node's location in cache.
+	 *
+	 * @param key Key for the value that is in the cache
+	 * @returns The value for the `key` that is defined in the parameter. If a value isn't stored with that
+	 * 			`key` then this will return undefined.
+	 */
 	get(key: string): any {
 		const cacheNode: ListNode = this.lookup.get(key);
 
@@ -42,10 +64,23 @@ class LFUCache {
 			return undefined;
 		}
 
-		this.increment(cacheNode, cacheNode.parent);
+		this.increment(cacheNode);
 		return cacheNode.value;
 	}
 
+	/**
+	 * Handles creating and inserting a new cache node in to the cache. When creating a frequency node for a
+	 * cache item that it's in the cache yet, then the frequency must be set to 1.
+	 *
+	 * The `oldParent` param will only be set if the key-value pair is already in cache and this function is
+	 * called from the `increment` function.
+	 *
+	 * @param key Key for the cache value
+	 * @param value Value that you want to cache
+	 * @param frequency Frequency of the cache node. This will be one when the cache node isn't in cache yet.
+	 * 					If the cache node is already cached then this will be the node's old frequency + 1.
+	 * @param oldParent Old parent of the cache node. This is only set if the cache node is already in cache.
+	 */
 	private createFrequencyNode(key: string, value: any, frequency: number, oldParent?: ListNode) {
 		const frequencyNode: ListNode = {
 			key: frequency,
@@ -71,17 +106,52 @@ class LFUCache {
 		}
 	}
 
-	private increment(node: ListNode, currentParent: ListNode) {
+	/**
+	 * Removes the `node` from it's current parent and inserts it in to a new frequency node
+	 * that has a frequency of n + 1
+	 *
+	 * @param node The node who's frequency you want to increment
+	 */
+	private increment(node: ListNode) {
+		const currentParent: ListNode = node.parent;
 		const nextNode: ListNode = currentParent.next;
+
+		// Remove the `node` from it's current parent
 		(currentParent.value as LinkedList).remove(node);
 
-		if (nextNode === undefined || (nextNode.key as number) != (currentParent.key as number) + 1) {
-			this.createFrequencyNode(node.key as string, node.value, (currentParent.key as number) + 1, currentParent);
+		// The frequency is kept in the key of the list node
+		const nextNodeFrequency: number = (nextNode.key as number) + 1;
+		const nextFrequency: number = (currentParent.key as number) + 1;
+
+		// If there isn't a node with the current frequency + 1 then create a new node otherwise insert
+		// the cache node to the next node
+		if (nextNode === undefined || nextNodeFrequency != nextFrequency) {
+			this.createFrequencyNode(node.key as string, node.value, nextFrequency, currentParent);
 		} else {
+			// This updates the cache node in the lookup map
 			const cacheNode: ListNode = { key: node.key, value: node.value, parent: nextNode };
 			this.lookup.set(node.key as string, cacheNode);
+
+			// And lastly add the cache node to the next frequency node
 			(nextNode.value as LinkedList).insertHead(cacheNode);
 		}
+	}
+
+	/**
+	 * Removes the least used item in the cache.
+	 *
+	 * This gets the head of the `frequencyList` and the head of the children in that frequency node,
+	 * then removes that node.
+	 *
+	 * Any of the items in the head of the `frequencyList` could be removed but it's just
+	 * easiest to remove the head.
+	 */
+	private removeLeastUsed() {
+		const leastUsedNode: ListNode = this.frequencyList.getHead();
+		const leastUsedCacheNode: ListNode = (leastUsedNode.value as LinkedList).getHead();
+		(leastUsedNode.value as LinkedList).remove(leastUsedCacheNode);
+		this.lookup.delete(leastUsedCacheNode.key as string);
+		this.size--;
 	}
 }
 
