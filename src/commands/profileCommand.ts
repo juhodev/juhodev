1b4a41617cc21a3ffd532db5f95bf645bd9cfa4f
Juhodev <jooo.dev@gmail.com>
2021-03-03 19:39:31 +0200

    Profile lookup with username and tag

--- src/commands/profileCommand.ts
+++ src/commands/profileCommand.ts
@@ -2,20 +2,46 @@ import { DMChannel, GuildMember, MessageEmbed, NewsChannel, TextChannel, User } 
 import { db } from '..';
 import { UserData, UserProfile } from '../api/types';
 import { getUserDataWithSnowflake, getUserProfile } from '../api/user';
+import { DBUser } from '../db/types';
+import { knex } from '../db/utils';
 import { isNil, msToTime } from '../utils';
 import { Command } from './types';
 
 const ProfileCommand: Command = {
 	execute: (channel, author, args, db) => {
+		if (args.length === 0) {
+			channel.send('!profile <snowflake/username#tag>');
+			return;
+		}
+
 		const user: string = args.shift();
 		sendUserProfile(channel, user);
 	},
 	alias: ['!profile', '!user'],
 };
 
-async function sendUserProfile(channel: TextChannel | DMChannel | NewsChannel, name: string) {
-	const userData: UserData = await getUserDataWithSnowflake(name);
+async function getSnowflake(name: string): Promise<string> {
+	const nameSplit: string[] = name.split('#');
+	const nameUpperCase: string = nameSplit[0].toUpperCase();
+	const tag: string = nameSplit[1];
 
+	const dbUser: DBUser = await knex<DBUser>('users')
+		.where({
+			discord_name_uppercase: nameUpperCase,
+			discord_tag: tag,
+		})
+		.first();
+
+	return dbUser?.snowflake;
+}
+
+async function sendUserProfile(channel: TextChannel | DMChannel | NewsChannel, name: string) {
+	let snowflake: string = name;
+	if (name.includes('#')) {
+		snowflake = await getSnowflake(name);
+	}
+
+	const userData: UserData = await getUserDataWithSnowflake(snowflake);
 	if (isNil(userData)) {
 		channel.send(`User ${name} not found!`);
 		return;
