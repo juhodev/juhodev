1c51dcce2e32042eb1e997cc4980f2dbe385d71c
nellipelkonen <62545812+nellipelkonen@users.noreply.github.com>
2021-03-29 00:40:56 +0300

    Update helpCommand.ts

--- src/commands/helpCommand.ts
+++ src/commands/helpCommand.ts
@@ -6,6 +6,10 @@ const HelpCommand: Command = {
 		const helpMessage: MessageEmbed = new MessageEmbed({
 			title: 'Baavo commands:',
 		})
+			.addField('!play <link> (Example: !play https://www.youtube.com/watch?v=dQw4w9WgXcQ)', 'DJ-Baavo will appear and play you some sick tunes.')
+			.addField('!play <keyword> (Example: !play sup yall)', 'DJ-Baavo plays you the top video on youtube search page of the keyword input.')
+			.addField('!play skip', 'Skips current song and plays the next one in the queue.')
+			.addField('!play q', 'Shows the queue of DJ-Baavo')
 			.addField('!baavo', 'Send baavos')
 			.addField('!profile <discord name and id> (Example#0000)', 'Discord stats of the user')
 			.addField('!times <page>', 'Leaderboard of time spent on voice channels')
