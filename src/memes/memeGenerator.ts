dacde15a3168aaeefd24109a9b771be70a4f4c87
Juhodev <jooo.dev@gmail.com>
2021-07-23 21:38:57 +0300

    fix rce real quick

--- src/memes/memeGenerator.ts
+++ src/memes/memeGenerator.ts
@@ -3,6 +3,7 @@ import { ChildProcess, exec } from 'child_process';
 
 export async function generateMeme(source: string, text: string): Promise<string> {
 	return new Promise((resolve, reject) => {
+		return;
 		const jarPath: string = path.resolve('jars', 'MemeGenerator-1.0.jar');
 		const command: string = `java -jar ${jarPath} -source ${source} -text ${text}`;
 
